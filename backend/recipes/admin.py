d58678c562d4a720f81436a19548e5cb100f58d7
pavellagerev <pavellagerev@yandex.ru>
2025-09-29 15:27:57 +0300

    Admin-100%

---
+++ backend/recipes/admin.py
@@ -0,0 +1,46 @@
+from django.contrib import admin
+from .models import (Tag, Ingredient, Recipe, RecipeIngredient,
+                     Favorite, ShoppingCart)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'measurement_unit')
+    search_fields = ('name',)
+
+
+class RecipeIngredientInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 1
+
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'author', 'get_favorites_count')
+    list_filter = ('author', 'name', 'tags')
+    search_fields = ('name', 'author__username')
+    inlines = (RecipeIngredientInline,)
+
+    @admin.display(description='Число добавлений в избранное')
+    def get_favorites_count(self, obj):
+        return obj.in_favorites.count()
+
+
+@admin.register(RecipeIngredient)
+class RecipeIngredientAdmin(admin.ModelAdmin):
+    list_display = ('recipe', 'ingredient', 'amount')
+
+
+@admin.register(Favorite)
+class FavoriteAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
+
+
+@admin.register(ShoppingCart)
+class ShoppingCartAdmin(admin.ModelAdmin):
+    list_display = ('user', 'recipe')
---
+++ backend/users/models.py
@@ -0,0 +1,65 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class User(AbstractUser):
+    """
+    Кастомная модель пользователя.
+    """
+    email = models.EmailField(
+        'Адрес электронной почты',
+        unique=True,
+        max_length=254
+    )
+    username = models.CharField(
+        'Уникальный юзернейм',
+        unique=True,
+        max_length=150
+    )
+    first_name = models.CharField(
+        'Имя',
+        max_length=150
+    )
+    last_name = models.CharField(
+        'Фамилия',
+        max_length=150
+    )
+
+    # Указываем `USERNAME_FIELD` для авторизации по email
+    USERNAME_FIELD = 'email'
+    # Указываем обязательные поля при создании суперпользователя
+    REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
+
+    class Meta:
+        verbose_name = 'Пользователь'
+        verbose_name_plural = 'Пользователи'
+        ordering = ('username',)
+
+    def __str__(self):
+        return self.username
+
+
+class Follow(models.Model):
+    """
+    Модель для подписок пользователей друг на друга.
+    """
+    user = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='follower',  # Тот, кто подписывается
+        verbose_name='Подписчик'
+    )
+    author = models.ForeignKey(
+        User,
+        on_delete=models.CASCADE,
+        related_name='following',  # Тот, на кого подписываются
+        verbose_name='Автор'
+    )
+
+    class Meta:
+        verbose_name = 'Подписка'
+        verbose_name_plural = 'Подписки'
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'author'],
+                                    name='unique_follow')
+        ]
