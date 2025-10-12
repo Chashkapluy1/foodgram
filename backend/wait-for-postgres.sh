26422440c07a22e9def038c10dd93116409148bf
pavellagerev <pavellagerev@yandex.ru>
2025-10-13 00:35:25 +0300

    Ревью_2_

--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-# wait-for-postgres.sh
-
 set -e
 
-host="$1"
-shift
+# Мы знаем, что хост всегда 'db', поэтому прописываем его здесь
+host="db"
 cmd="$@"
 
-# Используем pg_isready, так как это надежная клиентская утилита для проверки готовности Postgres
-until pg_isready -h "$host" -p "5432" -U "$POSTGRES_USER"; do
+until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
   sleep 1
 done
