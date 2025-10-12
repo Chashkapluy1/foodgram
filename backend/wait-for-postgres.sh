ae72cc4a0041b0ae3b458ecd8bbae3ef775062e7
pavellagerev <pavellagerev@yandex.ru>
2025-10-13 00:54:25 +0300

    Ревью_2_

--- backend/wait-for-postgres.sh
+++ backend/wait-for-postgres.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 set -e
 
-# Мы знаем, что хост всегда 'db', поэтому прописываем его здесь
-host="db"
+host="$1"
+shift
 cmd="$@"
 
 until PGPASSWORD=$POSTGRES_PASSWORD psql -h "$host" -U "$POSTGRES_USER" -d "$POSTGRES_DB" -c '\q'; do
