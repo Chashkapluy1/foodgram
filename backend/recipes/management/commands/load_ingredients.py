7d5b48346bdc8e5c7b71dc3804b4118927538683
pavellagerev <pavellagerev@yandex.ru>
2025-10-15 12:57:27 +0300

    РЕВЬЮ_2_INGRED

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -1,10 +1,40 @@
+import csv
+
+from django.core.management.base import BaseCommand
+
 from recipes.models import Ingredient
 
-from .base_loader import BaseLoader
 
+class Command(BaseCommand):
+    """Загрузка ингредиентов из CSV файла."""
+    help = 'Загрузка ингредиентов из data/ingredients.csv'
 
-class Command(BaseLoader):
-    """Загрузка ингредиентов из JSON файла."""
-    model = Ingredient
-    file_path = 'data/ingredients.json'
-    help = f'Загрузка ингредиентов из {file_path}'
+    def handle(self, *args, **kwargs):
+        file_path = 'data/ingredients.csv'
+        self.stdout.write(
+            self.style.SUCCESS(f'Начинаю загрузку из {file_path}')
+        )
+        try:
+            with open(file_path, 'r', encoding='utf-8') as file:
+                reader = csv.DictReader(file)
+                ingredients_to_create = [
+                    Ingredient(
+                        name=row['name'],
+                        measurement_unit=row['measurement_unit']
+                    ) for row in reader
+                ]
+                Ingredient.objects.bulk_create(
+                    ingredients_to_create,
+                    ignore_conflicts=True
+                )
+            self.stdout.write(
+                self.style.SUCCESS('Ингредиенты успешно загружены.')
+            )
+        except FileNotFoundError:
+            self.stdout.write(
+                self.style.ERROR(f'Файл {file_path} не найден.')
+            )
+        except Exception as e:
+            self.stdout.write(
+                self.style.ERROR(f'Ошибка при загрузке: {e}')
+            )
